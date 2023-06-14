2f47694280150f4680f4b9ea65d37ceb8db54840
Kamila Matejczuk <kamila.mate82@gmail.com>
2023-06-14 09:56:25 +0000

    My first commit

--- run.py
+++ run.py
@@ -1,55 +1,90 @@
+import gspread
 
-questions = ("What is the biggest island in the world?: ", 
-             "Which gas is the most in the Earth's atmosphere?: ", 
-             "Arsonphobia is a fear of: ", 
-             "What is the lifetime of a dragonfly?: ", 
-             "Which planet is close to the sun?: ", 
-             "What is the name of the god of the seas \
-              and oceans in Greek mythology?: ", 
-             "How many time zones does Australia have?: ", 
-             "What is the name of the largest country in the world?: ", 
-             "How many colors does the rainbow have?: : ", 
-             " What is the chemical symbol of silver?: ") 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 
-options = (("A Madagascar", "B. Java", "C. New Zealand" "D. Greenland"), 
-           ("A. Oxygen",  "B. Nitrogen", "C. Carbon dioxide", "D. Ozone"), 
-           ("A. Spiders", "B. Water ", "C. Fire", "D. Poison"), 
-           ("A. 24 hours", "B. One week", "C. One month", "D. Six months"), 
-           ("A. Mars", "B. Venus ", "C. Earth", "D. Mercury"), 
-           ("A. Poseidon", "B. Apollo", "C. Zeus", "D. Dionysus"), 
-           ("A. 2", "B. 3", "C. 4", "D. 1"), 
-           ("A. Canada", "B. USA", "C. Russia", "D. India"), 
-           ("A. Five", "B. Six", "C. Seven", "D. Eight"),
-           ("A. Na", "B. Fa", "C. Cu", "D. Ag")) 
+# intro text 
+   
+print("Welcome to Knowledge Quiz")
+print("-------------------------")
+print("The quiz has 10 questions about general knowledge ")
+print("-------------------------")
 
-answers = ("D", "B", "C", "A", "D", "A", "B", "C", "C", "D")
-guesses = []
-points = 0
-question_num = 0
+# Ask if player wants to start game
 
-for questions in questions:
-    print("-----------------------")
-    print(questions)
-    for option in options[question_num]:
-        print(option)
+players_name = ""
+players_name = input("Please enter your name: \n")
+print("Hello "+str(players_name)+"", "I wish you the best of luck!\n")
 
-    guess = input("Enter (A, B, C, or D): \n").upper()
-    guesses.append(guess)
-    if guess == answers[question_num]:
-        points = points + 1
-        print("Good answer")
+def start_game():
+
+    responses = []
+    correct_responses = 0
+    question_num = 1
+
+
+    for key in questions:
+	    print(" . . . . . . . . . . . . . . . . . ")
+	    print(key)
+		for i in options[question_num-1]:
+            print(i)
+
+		while True:
+	        reply = input("Choose (A, B, C or D): ")
+            reply = reply.upper()
+            responses.append(reply)
+		    if reply not in (A, B, C or D)
+		        print("Wrong choice, the only options are A, B, C, or D")		    
+            else:
+                break
+            correct_responses += verify_score(responses.get(key),reply)
+            question_num += 1
+        
+		
+
+def verify_score(score,reply):
+    
+    if score == reply:
+        print(" Good answer")
+        return 1
     else:
-        print("Wrong answer")
-
-    question_num = question_num + 1
-
+        print("This is incorrect answer")
+        return 0
+        
 def show_score():
+    pass
+def play_agian():
+    pass
 
 
-    print(".............................")
-    print("        Your score           ")
-    print(".............................")
 
 
-    percentage = (correct_answer/10) * 100
-    print("you got " + str(percentage) + "% of good answers!")
+# Quiz questions and answers
+
+questions = {
+    "1: What is the biggest island in the world?": "D",
+    "2: Which gas is the most in the Earth's atmosphere?": "B",
+    "3: Arsonphobia is a fear of:": "C",
+    "4: What is the lifetime of a dragonfly?": "A",
+    "5: Which planet is close to the sun?": "D",
+    "6: What is the name of the god of the seas and oceans in Greek mythology?": "A",
+    "7: How many time zones does Australia have?": "B",
+    "8: What is the name of the largest country in the world?": "C",
+    "9: How many colors does the rainbow have?": "C",
+    "10: What is the chemical symbol of silver?": "D"
+    }
+
+
+options = [["A Madagascar", "B. Java", "C. New Zealand", "D. Greenland"],
+           ["A. Oxygen",  "B. Nitrogen", "C. Carbon dioxide", "D. Ozone"],
+           ["A. Spiders", "B. Water ", "C. Fire", "D. Poison"],
+           ["A. 24 hours", "B. One week", "C. One month", "D. Six months"],
+           ["A. Mars", "B. Venus ", "C. Earth", "D. Mercury"],
+           ["A. Poseidon", "B. Apollo", "C. Zeus", "D. Dionysus"],
+           ["A. 2", "B. 3", "C. 4", "D. 1"],
+           ["A. Canada", "B. USA", "C. Russia", "D. India"],
+           ["A. Five", "B. Six", "C. Seven", "D. Eight"],
+           ["A. Na", "B. Fa", "C. Cu", "D. Ag"]]
