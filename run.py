a80de1fa9cd3709ea1cb294fafb47e5cc069ba26
KamilaAzul <>
2023-11-23 12:36:35 +0000

    Wire up APIS and confirm acces to google sheet

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import random
+import gspread
+from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -6,6 +8,14 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('knowledge_quiz')
+
+leaderboard = SHEET.worksheet('leaderboard')
+
+data = leaderboard.get_all_values()
 
 # Welcoming text
    
